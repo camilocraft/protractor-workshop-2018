b8559181db1aab921c5abb07347f18497dff6002
camilocraft <camilocraft@users.noreply.github.com>
2018-10-26 17:16:41 -0500

    explicit-wait (#13)

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,7 +5,7 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
-  getPageTimeout: 500,
+  getPageTimeout: 30000,
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
   },
@@ -18,6 +18,6 @@ export const config: Config = {
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
-    browser.manage().timeouts().implicitlyWait(1000);
+    browser.manage().timeouts().implicitlyWait(0);
   }
 };

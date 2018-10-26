3dddae5ffe6b71d799c9ba7ba22a6ad6d913ca82
camilocraft <camilocraft@users.noreply.github.com>
2018-10-26 17:14:35 -0500

    timeout (#11)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -6,6 +6,9 @@ export const config: Config = {
   specs: ['../test/*.spec.js'],
   getPageTimeout: 500,
   SELENIUM_PROMISE_MANAGER: false,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();

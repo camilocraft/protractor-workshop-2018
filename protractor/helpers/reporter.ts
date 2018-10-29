102aa4be2bbdafcd868e8dd30d7db124d49d0edb
camilocraft <camilocraft@users.noreply.github.com>
2018-10-29 14:46:55 -0500

    jasmine-awesome-report (#16)

--- protractor/helpers/reporter.ts
+++ protractor/helpers/reporter.ts
@@ -1,9 +1,16 @@
 import { SpecReporter } from 'jasmine-spec-reporter';
+const { AwesomeReport } = require('jasmine-awesome-report');
 
 export let reporter = () => {
   jasmine.getEnv().addReporter(new SpecReporter({
     spec: {
       displayStacktrace: true
     }
+  }));
+
+  jasmine.getEnv().addReporter(AwesomeReport.getReport({
+    fullPath: 'reports',
+    fileName: 'report',
+    merge: true
   }));
 };
